9884d28fc1b8edec820b52c8da26627ef39d3e79
Jesús Mesa González <sulimesa@gmail.com>
2018-11-06 10:18:44 +0100

    Añadidos todos los ficheros de la aplicación

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
 
-express()
+// Vector para almacenar nombres.
+var nombres = new Array;
+
+// Operación PUT para introducir un nuevo nombre en el
+// vector.
+app.put('/nombre/:nomb', function (req, res) {
+
+	// Almacenar el nombre en el vector.
+	nombres.push(req.params.nomb);  
+	res.send("Nombre almacenado");
+});
+
+// Operación GET para consultar los nombres introducidos
+// hasta el momento.
+app.get('/nombres', function(req,res){
+
+	// Responder al cliente con los nombres del vector.
+	res.send(nombres);
+});
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+module.exports = app
